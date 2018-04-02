3d35a4b37870bfb4d232acdb75342b3ab0758741
Harbir4 <harbir478@gmail.com>
2018-04-02 14:12:41 -0700

    axios url change

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '04e403f4190cc5ad12e50554fbcea22c';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
